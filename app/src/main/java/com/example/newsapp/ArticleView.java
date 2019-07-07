daca3bfd7efb0e2e796dc1419c15adf79aab5d38
Debjoy Bhowal <41815782+Debjoy@users.noreply.github.com>
2019-07-07 10:55:32 +0530

    added functions for accessing the add button

--- app/src/main/java/com/example/newsapp/ArticleView.java
+++ app/src/main/java/com/example/newsapp/ArticleView.java
@@ -2,8 +2,11 @@ package com.example.newsapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -19,6 +22,17 @@ public class ArticleView extends AppCompatActivity {
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.details_container,new Fragment_Article(n.getArticle_url()));
         ft.commit();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_item1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuitem){
+        Toast.makeText(this, "Menu clicked", Toast.LENGTH_SHORT).show();
+        return true;
     }
     @Override
     protected void onDestroy() {
